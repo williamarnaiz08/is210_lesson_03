dced1ae92f4ff493672d62de6c4f63f8c71bf0da
William Arnaiz <william.arnaiz@spsmail.cuny.edu>
2014-09-15 21:46:42 -0400

    Asks for users name and age

---
+++ task_01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+QUESTION_01 = raw_input("What is your name? ")
+
+print "Hello, " + QUESTION_01 + "!"
+
+QUESTION_02 = raw_input("How old are you? ")
+
+int(QUESTION_02)
+
+print "Wow, " + QUESTION_02 + " is pretty old, " + QUESTION_01 + " !"
